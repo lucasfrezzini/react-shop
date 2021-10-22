8a88492f6cc784bd2c943c4d06bb0714ada216bc
lucasfrezzini <luquiitas.f@gmail.com>
2021-10-22 09:26:29 -0300

    Corrección de imagenes, links, useState y useEffect en menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import '../styles/Menu.scss';
+import '@styles/Menu.scss';
 
 const Menu = () => {
 	return (
 		<div className="Menu">
 			<ul>
 				<li>
-					<a href="/" className="title">My orders</a>
+					<a href="/orders" className="title">My orders</a>
 				</li>
 				<li>
-					<a href="/">My account</a>
+					<a href="/account">My account</a>
 				</li>
 				<li>
 					<a href="/">Sign out</a>
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,16 @@ module.exports = {
 	mode: 'development',
 	resolve: {
 		extensions: ['.js', '.jsx'],
+		alias: {
+			'@components': path.resolve(__dirname, 'src/components/'),
+			'@containers': path.resolve(__dirname, 'src/containers/'),
+			'@pages': path.resolve(__dirname, 'src/pages/'),
+			'@routes': path.resolve(__dirname, 'src/routes/'),
+			'@hooks': path.resolve(__dirname, 'src/hooks/'),
+			'@styles': path.resolve(__dirname, 'src/styles/'),
+			'@icons': path.resolve(__dirname, 'src/assets/icons/'),
+			'@logos': path.resolve(__dirname, 'src/assets/logos/'),
+		}
 	},
 	module: {
 		rules: [
@@ -37,6 +47,10 @@ module.exports = {
 					"css-loader",
 					"sass-loader",
 				],
+			},
+			{
+				test: /\.(svg|jpg|png|gif)$/,
+				type: 'asset',
 			}
 		]
 	},
